37775e7c405dedf583295f909baffc817b315545
Namrata Agarwal <namrata.agarwal@morningstar.com>
2019-07-22 18:16:25 +0530

    Select by id

--- demo/src/main/java/com/example/demo/resolvers/Query.java
+++ demo/src/main/java/com/example/demo/resolvers/Query.java
@@ -2,6 +2,7 @@ package com.example.demo.resolvers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -30,5 +31,24 @@ public class Query implements GraphQLQueryResolver {
 		return pets;
 		
 	}
+	
+	public List<Pet> pets(Long id) {
+        List<Pet> pets = new ArrayList<>();
 
+		Pet pet1 = new Pet();
+
+		pet1.setId(1);
+		pet1.setName("Billi");
+		pet1.setType(Animal.CAT);
+		pet1.setAge(10);
+		pets.add(pet1);
+		Pet pet2 = new Pet();
+		pet2.setId(2);
+		pet2.setName("kutta");
+		pet2.setType(Animal.DOG);
+		pet2.setAge(14);
+		pets.add(pet2);
+		return pets.stream().filter(a ->a.getId()==id).collect(Collectors.toList());
+		
+	}
 }
